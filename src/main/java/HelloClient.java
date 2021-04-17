c10361694c0913c3a50166bffefbc81f83fbc723
Koenraad007 <koenvanaken1999@gmail.com>
2021-04-17 14:35:50 +0200

    Changes to HelloClient and HelloServer

---
+++ src/main/java/HelloClient.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.*;
+
+public class HelloClient {
+
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port){
+        try {
+            clientSocket = new Socket(ip, port);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        }
+        catch (Exception e){
+            System.out.printf("Something went wrong: " + e.toString());
+        }
+    }
+
+    public String sendMessage(String message){
+        try {
+            out.println(message);
+            String respons = in.readLine();
+            return respons;
+        }
+        catch (Exception e) {
+            System.out.println("Something went wrong: " + e.toString());
+            return "Oops";
+        }
+    }
+
+    public void stop(){
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+        }
+        catch (Exception e) {
+            System.out.println("Something went wrong: " + e.toString());
+        }
+    }
+
+}
